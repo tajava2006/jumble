2ee9037322b174404365a8095691b1f99b9b3015
bitcoinuser <bitcoinuser@users.noreply.github.com>
2025-08-30 12:09:49 -0300

    feat: update pt-BR.ts (#510)

--- src/i18n/locales/pt-BR.ts
+++ src/i18n/locales/pt-BR.ts
@@ -370,7 +370,7 @@ export default {
     Apply: 'Aplicar',
     Reset: 'Redefinir',
     'Share something on this Relay': 'Compartilhe algo neste Relay',
-    'Try deleting this note': 'Tente excluir esta nota',
+    'Try deleting this note': 'Solicitar exclusão desta nota',
     'Deletion request sent to {{count}} relays': 'Pedido de exclusão enviado para {{count}} relays'
   }
 }
